ad8108cd09181e56485529ca0e41b114d5c139b1
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-13 09:47:23 -0800

    adds jwt auth middleware. returns clearer error message for duoplicate username on account registration

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -14,27 +15,54 @@ router.post('/register', (req, res) => {
       res.status(201).json(saved);
     })
     .catch(error => {
-      res.status(500).json(error);
+      if (error.code === "SQLITE_CONSTRAINT") {
+        res
+          .status(401)
+          .json({
+            message: `An account with that username already exists`,
+            error
+          });
+      } else {
+        res.status(500).json(error);
+      }
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // produce a token
+        const token = getJwtToken(user.username);
+        // send the token to the client
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
 });
+
+function getJwtToken(username) {
+  const payload = {
+    username,
+    role: "student" //this will come from the db. this is just an example
+  };
+  const secret = process.env.JWT_SECRET || "is it secret is it safe?";
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,20 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  if (token) {
+    // check that the token is valid
+    const secret = process.env.JWT_SECRET || "is it secret is it safe?";
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        // bad panda, token has been tampered with
+        res.status(401).json({ message: "Invalid Credentials" });
+      } else {
+        next();
+      }
+    });
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: "No credentials provided" });
   }
 };
